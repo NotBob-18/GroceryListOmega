603ae042fbb0dbc0cfe92525d432f89da56a235a
Michael Masala <michaelmasala18@gmail.com>
2024-07-11 15:22:49 +0200

    first commit

---
+++ src/main/java/com/GroceryListOmega/GroceryListOmegaApplication.java
@@ -0,0 +1,13 @@
+package com.GroceryListOmega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryListOmegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryListOmegaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GroceryListOmega/GroceryListOmegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GroceryListOmega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryListOmegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
